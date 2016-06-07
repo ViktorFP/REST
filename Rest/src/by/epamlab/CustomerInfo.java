d9df18ba8ad94aee38b3e06f5bd3c70a00f7d3cd
ViktorFP <VikPr_85@mail.ru>
2016-06-07 13:52:56 +0300

    +customer

--- Rest/src/by/epamlab/CustomerInfo.java
+++ Rest/src/by/epamlab/CustomerInfo.java
@@ -1,23 +1,37 @@
 package by.epamlab;
 
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.xml.sax.SAXException;
+
+import by.epamlab.beans.ifaces.IReservationDAO;
+import by.epamlab.beans.reservations.Reservation;
+import by.epamlab.beans.reservations.customer.Customer;
+import by.epamlab.factories.ReservationFactory;
+
 @Path("CustomerInfoService")
 public class CustomerInfo {
 
-	/*http://localhost:8080/Rest/rest/CustomerInfoService/customer/[reservation]*/
+	// http://localhost:8080/Rest/rest/CustomerInfoService/customer/[reservationFileName]
+
 	@GET
-	@Path("/customer/{reservation}")
-	@Produces(MediaType.TEXT_HTML)
-	public String customer(@PathParam("reservation") String reservation) {
-		/*
-		 * jar модуля; получение через jndi объекта (краткое. все на JBoss);
-		 * getFile(....) ...
-		 */
-		return "testCustomer";
+	@Path("/customer/{reservationFileName}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String customer(@PathParam("reservationFileName") String reservationFileName) {
+		Reservation reservation = null;
+		IReservationDAO reservationDAO = ReservationFactory.getClassFromFactory();
+		try {
+			reservation = reservationDAO.getReservation(reservationFileName);
+		} catch (IOException | SAXException e) {
+			return e.getMessage();
+		}
+		Customer customer = reservation.getCustomer();
+		return customer.getFirstName() + " " + customer.getLastName();
 	}
 }
